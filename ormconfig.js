8584eb434ef790abd12df0807097c618cb6a45f5
ArturS <arturs.dasilva21@gmail.com>
2022-06-13 16:52:45 -0300

    ajuste heroku v3

--- ormconfig.js
+++ ormconfig.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 
 module.exports = {
-    type: process.env.DATABASE_TYPE,
-    url: process.env.DATABASE_URL,
+    // type: process.env.DATABASE_TYPE,
+    type: "postgres",
+    url: "postgres://mvqilnnntbnglv:e7dd52bc9b479e262710559f2495003ed8a9b2405e7de911ea77b89351bb384c@ec2-34-236-94-53.compute-1.amazonaws.com:5432/d57r8s0o2fv80g",
+    // url: process.env.DATABASE_URL,
     logging: false,
     extra: {
         ssl: {
