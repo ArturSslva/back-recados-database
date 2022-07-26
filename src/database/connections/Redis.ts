596b35262088f4cfe8713f2bef998366087928a8
ArturS <arturs.dasilva21@gmail.com>
2022-07-25 23:27:30 -0300

    adiciona configuracoes do cache

---
+++ src/database/connections/Redis.ts
@@ -0,0 +1,29 @@
+import IORedis from "ioredis";
+
+export default class Redis {
+    private static instance: IORedis.Redis;
+
+    private constructor() {}
+
+    static getInstance() {
+        if (!Redis.instance) {
+            const redis = new Redis();
+            Redis.instance = redis.openConnection();
+        }
+        return Redis.instance;
+    }
+
+    private openConnection() {
+        try {
+                        /* HEROKU
+            return new IORedis(process.env.REDIS_URL, , {
+                tls: {
+                  rejectUnauthorized: false
+                }
+            });*/
+            return new IORedis(process.env.REDIS_URL);
+        } catch(error) {
+            throw (`Erro ao se conectar ao Redis: ${error}`);
+        }
+    }
+}
---
+++ src/database/repositories/cache.ts
@@ -0,0 +1,32 @@
+import IORedis from "ioredis";
+import { CacheRepositoryInterface } from "../../contracts/repositories/cache";
+import Redis from "../connections/Redis";
+
+export class CacheRepository implements CacheRepositoryInterface {
+    private readonly redis: IORedis.Redis;
+
+    constructor() {
+        this.redis = Redis.getInstance();
+    }
+
+    async save(key: string, value: any) {
+        return await this.redis.set(key, JSON.stringify(value));
+    }
+
+    async saveExpiration(key: string, value: any, ttl: number) {
+        return await this.redis.set(key, JSON.stringify(value),'EX', ttl);
+    }
+
+    async find(key: string) {
+        const value = await this.redis.get(key);
+
+        return value ? JSON.parse(value) : null;
+    }
+
+    async delete(key: string) {
+        const result = await this.redis.del(key);
+
+        return result !==0;
+    }
+
+}
